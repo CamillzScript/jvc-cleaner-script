5bff0f54a51c6b0a14ba610d2cc9d1a7fe2a0572
CamillzScript <borninweb.fr@gmail.com>
2022-08-22 05:13:45 +0200

    add loading message && change version

--- jvc-cleacer-script.user.js
+++ jvc-cleacer-script.user.js
@@ -6,7 +6,7 @@
 // @icon         https://www.jeuxvideo.com/favicon.png
 // @license      MIT
 //
-// @version      1.0.3
+// @version      1.0.5
 // @downloadURL  https://raw.githubusercontent.com/CamillzScript/jvc-cleaner-script/main/jvc-cleacer-script.user.js
 // @updateURL    https://raw.githubusercontent.com/CamillzScript/jvc-cleaner-script/main/jvc-cleacer-script.user.js
 //
@@ -21,7 +21,22 @@
 // @noframes
 // ==/UserScript==
 
-GM_addElement('script', {
-  src: 'https://raw.githubusercontent.com/CamillzScript/jvc-cleaner-script/main/modules/navitem.mjs',
-  type: 'text/script'
-})
+console.log('JVC Cleaner Loaded!');
+
+/** **************************************************************************************************************
+ *  FUNCTION
+ ************************************************************************************************************** */
+ () => {
+   const position = document.querySelector('.header__globalUser');
+   let htmlMenuElement = '';
+   htmlMenuElement += '<div class="headerAccount headerAccount--jvc-cleaner-script dropdown">';
+   htmlMenuElement += '<span class="headerAccount__notif js-header-menu-dropdown js-header-notif">';
+   htmlMenuElement += '<i class="icon-check-jv"></i>';
+   htmlMenuElement += '</span>';
+   htmlMenuElement += '<div class="dropdown-menu headerAccount__dropdownContainer">';
+   htmlMenuElement += '<div id="jvc-cleaner-script-menu"></div>';
+   htmlMenuElement += '</div>';
+   htmlMenuElement += '</div>';
+
+   position.append = htmlMenuElement;
+ }
