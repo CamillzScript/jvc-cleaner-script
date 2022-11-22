26ea7a999bb04de8e6fc993a98a8eb5cf6764180
CamillzScript <borninweb.fr@gmail.com>
2022-11-22 02:05:52 +0100

    Update jvc-cleacer-script.user.js

--- jvc-cleacer-script.user.js
+++ jvc-cleacer-script.user.js
@@ -5,7 +5,7 @@
 // @supportURL   https://github.com/CamillzScript/jvc-cleaner-script/issues
 // @license      MIT
 //
-// @version      1.0.42
+// @version      1.0.43
 // @downloadURL  https://raw.githubusercontent.com/CamillzScript/jvc-cleaner-script/main/jvc-cleacer-script.user.js
 // @updateURL    https://raw.githubusercontent.com/CamillzScript/jvc-cleaner-script/main/jvc-cleacer-script.user.js
 //
